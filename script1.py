dcdee99d5ee62d8352be4a3e6a592d5c66b7a1f0
crimgene1 <crimgene1@gmail.com>
2021-05-20 21:53:37 +0800

    Version 1 by Raymond A. Ombrog

---
+++ script1.py
@@ -0,0 +1,6 @@
+def personal_details():
+    name, age = "Raymond Ombrog", 20
+    address = "North Fairview Quezon City"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+personal_details()
